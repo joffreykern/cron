25c54316cda090a7ad2c2fceb6026d783f95a438
Joffrey Kern <joffrey.kern@asos.com>
2019-07-06 12:04:56 +0100

    Parametrise test and improve code readability

--- JKCron.Tests/CronShould.cs
+++ JKCron.Tests/CronShould.cs
@@ -19,16 +19,26 @@ namespace JKCron.Tests
         }
 
         [Test]
-        public void ReturnTheCorrectValuesForTheGivenParameters()
+        [TestCase("9", "12", "15", "5", "1", "/usr/bind/find")]
+        [TestCase("1", "2", "3", "4", "5", "/usr/bind/dotnet")]
+        [TestCase("60", "24", "31", "12", "7", "/usr/bind/helloworld")]
+        [TestCase("0", "0", "0", "0", "0", "/usr/bind/hello")]
+        public void ReturnTheCorrectValuesForTheGivenParameters(
+            string minute,
+            string hour,
+            string dayOfMonth,
+            string month,
+            string dayOfWeek,
+            string command)
         {
-            this.Given(_ => ParametersAre(new string[] { "09", "12", "15", "05", "1", "/usr/bind/find" }))
+            this.Given(_ => ParametersAre(new string[] { minute, hour, dayOfMonth, month, dayOfWeek, command }))
                 .When(_ => ExecuteCron())
-                .Then(_ => OutputShouldReturn("minute \t\t09\r\n" +
-                                              "hour \t\t12\r\n" +
-                                              "day of month \t15\r\n" +
-                                              "month \t\t05\r\n" +
-                                              "day of week \t1\r\n" +
-                                              "command \t/usr/bind/find\r\n"))
+                .Then(_ => OutputShouldReturn($"minute \t\t{minute}\r\n" +
+                                              $"hour \t\t{hour}\r\n" +
+                                              $"day of month \t{dayOfMonth}\r\n" +
+                                              $"month \t\t{month}\r\n" +
+                                              $"day of week \t{dayOfWeek}\r\n" +
+                                              $"command \t{command}\r\n"))
                 .BDDfy();
         }
 
